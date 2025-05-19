9bb3db3a4d0d55ee17d6d07a0ed91d9cdc6b6a5a
Notsume <adahorapirola@gmail.com>
2025-05-19 20:14:32 -0300

    header sidebar

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,21 +3,29 @@ import MusicDot from "./musicdotlogo";
 export default function Header() {
     return (
         <>
-            <header className="">
-                <figure>
-                    <MusicDot></MusicDot>
-                </figure>
-                <ul className="m-8">
-                    <li className="">
-                        <a href="">Explore Musicas</a>
-                    </li>
-                    <li>
-                        <a href="">Suas Músicas</a>
-                    </li>
-                    <li>
-                        <a href="">Suas Playlists</a>
-                    </li>
-                </ul>
+            <header>
+                <div className="p-8">
+                    <figure>
+                        <MusicDot></MusicDot>
+                    </figure>
+                    <ul>
+                        <li>
+                            <h3 className="font-bold">Explore Musicas</h3>
+                            <ul>
+                                <li><a href="">Descubra</a></li>
+                                <li><a href="">Gêneros</a></li>
+                                <li><a href="">Top Charts</a></li>
+                                <li><a href="">Rádio</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="">Suas Músicas</a>
+                        </li>
+                        <li>
+                            <a href="">Suas Playlists</a>
+                        </li>
+                    </ul>
+                </div>
             </header>
         </>
     )
